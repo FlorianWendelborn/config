bebb02c90dd797ad7bee75698431ab94c64cc8f0
dodekeract <dodekeract@users.noreply.github.com>
2016-09-19 13:15:34 +0200

    improve syncthing

--- docker/syncthing/install.sh
+++ docker/syncthing/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# folders
+
+mkdir -p /srv/docker/syncthing/config
+mkdir -p /srv/docker/syncthing/data
+
+# docker
+
+docker pull linuxserver/syncthing
+
 # nginx
 
 cp ./nginx.conf /srv/docker/nginx/config/sites-enabled/syncthing
-
-# restart nginx
 
 docker restart nginx
---
+++ docker/syncthing/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# docker
+
+docker run \
+	--name=syncthing
+	-v /srv/docker/syncthing/config:/config \
+	-v /srv/docker/syncthing/data:/data \
+	-e PGID=$GID -e PUID=$UID \
+	-p 8384:8384 -p 22000:22000 -p 21027:21027/udp \
+	linuxserver/syncthing

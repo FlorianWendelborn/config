ca765aac8ccf7574d0e0180fb89f03210ce43ed7
dodekeract <dodekeract@users.noreply.github.com>
2016-09-22 01:24:45 +0200

    add manta

--- projects/install.sh
+++ projects/install.sh
@@ -11,17 +11,28 @@ mkdir -p $TARGET
 
 pushd $TARGET
 
-git clone https://github.com/dodekeract/mandelbrot
-git clone https://github.com/dodekeract/particles
-git clone https://github.com/dodekeract/line-replace-fractals
-git clone https://github.com/dodekeract/spirograph
-git clone https://github.com/dodekeract/spot-the-difference
+	git clone https://github.com/dodekeract/mandelbrot
+	git clone https://github.com/dodekeract/particles
+	git clone https://github.com/dodekeract/line-replace-fractals
+	git clone https://github.com/dodekeract/spirograph
+	git clone https://github.com/dodekeract/spot-the-difference
+
+	# manta
+
+	git clone https://github.com/dodekeract/manta-config-engine-app
+	pushd manta-config-engine-app
+		npm install
+		npm run build
+		mv build ../manta
+	popd
+	rm -rf manta-config-engine-app
 
 popd
 
 # copy nginx config
 
 cp ./nginx.conf $SITES/projects.dodekeract.com
+cp ./nginx.conf $SITES/projects-legacy
 
 # restart nginx
 

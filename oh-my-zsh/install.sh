9f47a0d70e19275ca415da29d91447a2bf18b80b
dodekeract <florianwendgithub@gmail.com>
2016-10-08 15:52:15 +0200

    add .zshrc

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,6 +2,13 @@
 
 OH_MY_ZSH="/opt/oh-my-zsh"
 
+# zshrc
+
+cp .zshrc ~/.zshrc
+chown dodekeract:dodekeract .zshrc
+
+# theme
+
 cp dodekeract.zsh-theme "$OH_MY_ZSH/themes/dodekeract.zsh-theme"
 
 chown root:root "$OH_MY_ZSH/themes/dodekeract.zsh-theme"

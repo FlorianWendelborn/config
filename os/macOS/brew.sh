9507a343966394d2e2f1878353e42ca015d51ba7
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-01-23 22:50:16 +0100

    Update brew.sh

--- os/macOS/brew.sh
+++ os/macOS/brew.sh
@@ -2,7 +2,7 @@
 
 brew cask install syncthing-bar
 brew cask install teamviewer
-brew install Caskroom/versions/google-chrome-canary
+brew cask install google-chrome-canary
 brew install gnu-prolog
 brew cask install disablemonitor
 brew cask install paw

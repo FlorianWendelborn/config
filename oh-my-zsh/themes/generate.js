62f53b489a57b017dda56d8e39a638ab303f4ec4
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-01-28 01:31:53 +0100

    …literally

--- oh-my-zsh/themes/generate.js
+++ oh-my-zsh/themes/generate.js
@@ -17,9 +17,7 @@ const systems = {
 }
 
 // theme
-const generateTheme = ({color, ...rest}) => ({
-	...rest,
-	color,
+const generateTheme = properties => Object.assign(properties, {
 	theme: `# auto-generated zsh-theme
 
 local ret_status="%(?::%{$fg_bold[red]%}x )"
@@ -29,17 +27,15 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}*%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-SSH="%{$fg[${color}]%}%{\${SSH_TTY:+[%n@%m] }%}%{$reset_color%}"
+SSH="%{$fg[${properties.color}]%}%{\${SSH_TTY:+[%n@%m] }%}%{$reset_color%}"
 END="%(!.%{$fg[red]%}❯❯❯%{$reset_color%}.%{$fg[green]%}❯%{$reset_color%})"
 
-PROMPT='\${SSH}\${ret_status}%{$reset_color%}%{$fg[${color}]%}%1~%{$reset_color%}%{$fg[red]%} %{$reset_color%}$(git_prompt_info)%{$reset_color%}\${END} '`
+PROMPT='\${SSH}\${ret_status}%{$reset_color%}%{$fg[${properties.color}]%}%1~%{$reset_color%}%{$fg[red]%} %{$reset_color%}$(git_prompt_info)%{$reset_color%}\${END} '`
 })
 
 // destination
-const generateDestination = ({name, ...rest}) => ({
-	...rest,
-	name,
-	destination: `${__dirname}/${name}.zsh-theme`
+const generateDestination = properties => Object.assign(properties, {
+	destination: `${__dirname}/${properties.name}.zsh-theme`
 })
 
 // filesystem
@@ -48,7 +44,7 @@ const write = ({destination, theme}) => fs.writeFile(destination, theme)
 // run it
 Object
 	.keys(systems)
-	.map(name => ({name, ...systems[name]})
+	.map(name => Object.assign({name}, systems[name]))
 	.map(generateTheme)
 	.map(generateDestination)
 	.forEach(write)

c8a33eb4dab4317a3a82191ae37e446202c3cdb6
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-02-15 11:33:28 +0100

    Update brew.sh

--- os/macOS/brew.sh
+++ os/macOS/brew.sh
@@ -3,7 +3,6 @@
 brew cask install syncthing-bar
 brew cask install teamviewer
 brew cask install google-chrome-canary
-brew install gnu-prolog
 brew cask install disablemonitor
 brew cask install paw
 brew cask install tower

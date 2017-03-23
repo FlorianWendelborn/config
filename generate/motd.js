2a1768c16da15b3df33302e18958138d61bdb054
dodekeract <florianwendgithub@gmail.com>
2017-03-24 00:36:26 +0100

    improve motd

--- generate/motd.js
+++ generate/motd.js
@@ -3,7 +3,7 @@ module.exports = device => `#!/bin/zsh
 
 # calculate
 IP="$(hostname -I | sed -e 's/\\ /\\ \\ \\ \\ /g')"
-FIGLET="$(figlet -w 92 -f small ${device.hostname})"
+FIGLET="$(figlet -w 92 -f small ${device.fullName})"
 DISTRIBUTION="${device.commands.distribution}"
 ${device.hasDocker ? `DOCKER=$(echo "STATUS\\tNAME\\tIMAGE\\tPORTS\\n$(docker ps -a --format '{{.Status}}\\t{{.Names}}\\t{{.Image}}\\t{{.Ports}}')" | sed -e 's/\\/tcp/t/g' -e 's/\\/udp/u/g' -e 's/\\,\\ /\\ /g' -e 's/[-][>]/-/g' -e 's/0\\.0\\.0\\.\\0\\://g' -e 's/\\b\\([0-9]\\+\\)[-,\\n]\\1/\\1/g' | column -t -s $'\\t')` : '# no docker'}
 UPTIME=$(echo "$(uptime | cut -f4,5,6 -d' ' | cut -f1,2 -d',') hours" | sed -e 's/\\,\\ \\ hours//g')

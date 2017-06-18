e0ed945379f168b6942396cdbed9e0e75a465da4
dodekeract <dodekeract@users.noreply.github.com>
2017-06-18 22:43:31 +0200

    add mail, syncthing, web-components

--- legacy/teamspeak/install.js
+++
@@ -1,4 +0,0 @@
-STORAGE="/srv/docker/teamspeak"
-docker pull mbentley/teamspeak
-sudo mkdir -p "$STORAGE"
-docker run -d -v "$STORAGE:/data" --name teamspeak -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 mbentley/teamspeak

5aeac73154dc1231d1bdb53d56bc86f033764715
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-06-20 19:31:39 +0200

    Dock Additions

--- os/macOS/dock.sh
+++ os/macOS/dock.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# add spacer
+# defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
 defaults write com.apple.dock autohide-time-modifier -int 0
 defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.Dock showhidden -bool TRUE
 
 killall Dock

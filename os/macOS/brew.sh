8ef41447473e17e8619823ad2a10169129c0692a
Florian Wendelborn <dodekeract@users.noreply.gitub.com>
2017-06-23 16:06:06 +0200

    fix brew.sh

--- os/macOS/brew.sh
+++ os/macOS/brew.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-brew cask install aerial
+# brew cask install aerial
 brew cask install atom
-brew cask install cakebrew
+# brew cask install cakebrew
 brew cask install colorsnapper
-brew cask install disablemonitor
-brew cask install disk-inventory-x
-brew cask install gitify
+# brew cask install disablemonitor
+# brew cask install disk-inventory-x
+# brew cask install gitify
 brew cask install google-chrome-canary
 brew cask install iterm2
-brew cask install opera
+# brew cask install opera
 brew cask install paw
-brew cask install safari-technology-preview
-brew cask install spotify
-brew cask install steam
+# brew cask install safari-technology-preview
+# brew cask install spotify
+# brew cask install steam
 brew cask install syncthing-bar
 brew cask install teamspeak-client
 brew cask install teamviewer
 brew cask install tower
-brew cask install unetbootin
+# brew cask install unetbootin
 brew install ctop
-brew install m-cli
+# brew install m-cli
 
 # https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo

5f86feb129bda84581238e2273c49c62ffdd1001
Florian Wendelborn <github@dodekeract.com>
2017-09-26 02:49:08 +0200

    Remove Bartender Cask

--- os/macOS/brew.sh
+++ os/macOS/brew.sh
@@ -19,7 +19,6 @@ brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch 
 
 ## other
 brew install ctop
-brew cask install bartender
 brew cask install disk-inventory-x
 brew cask install syncthing-bar
 brew cask install teamspeak-client

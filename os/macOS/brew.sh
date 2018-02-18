cc1c81102ab5e4198d7b232a514377c60c601705
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-02-18 02:38:29 +0100

    Update brew.sh

--- os/macOS/brew.sh
+++ os/macOS/brew.sh
@@ -20,6 +20,9 @@ brew cask install iina qsyncthingtray webtorrent
 ## [quicklook](https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
+## ffmpeg https://gist.github.com/clayton/6196167#gistcomment-2343443
+brew install ffmpeg --with-chromaprint --with-fdk-aac --with-libass --with-librsvg --with-libsoxr --with-libssh --with-tesseract --with-libvidstab --with-opencore-amr --with-openh264 --with-openjpeg --with-openssl --with-rtmpdump --with-rubberband --with-sdl2 --with-snappy --with-tools --with-webp --with-x265 --with-xz --with-zeromq --with-zimg
+
 ## other
 brew install ctop
 brew cask install disk-inventory-x

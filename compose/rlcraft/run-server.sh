fcb8fdd30202f7ae44e514d33d8432ad3939414b
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2021-01-04 16:05:27 +0100

    add rlcraft

---
+++ compose/rlcraft/run-server.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DEFAULT_RAM=4G
+
+if [ ! -f eula.txt ]; then
+     if [ "$EULA" != "" ]; then
+          echo "eula=$EULA" > eula.txt
+     else
+          echo "Please provide the EULA environment variable."
+          echo "Note: By providing EULA=TRUE you agree to the EULA at https://account.mojang.com/documents/minecraft_eula."
+          exit 1
+     fi
+fi
+
+# Parameters from aikar's post:
+# https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
+java -Xms${RAM:-$DEFAULT_RAM} -Xmx${RAM:-$DEFAULT_RAM} \
+     -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+     -XX:MaxGCPauseMillis=200 \
+     -XX:+UnlockExperimentalVMOptions \
+     -XX:+DisableExplicitGC \
+     -XX:+AlwaysPreTouch \
+     -XX:G1NewSizePercent=30 \
+     -XX:G1MaxNewSizePercent=40 \
+     -XX:G1HeapRegionSize=8M \
+     -XX:G1ReservePercent=20 \
+     -XX:G1HeapWastePercent=5 \
+     -XX:G1MixedGCCountTarget=4 \
+     -XX:InitiatingHeapOccupancyPercent=15 \
+     -XX:G1MixedGCLiveThresholdPercent=90 \
+     -XX:G1RSetUpdatingPauseTimePercent=5 \
+     -XX:SurvivorRatio=32 \
+     -XX:+PerfDisableSharedMem \
+     -XX:MaxTenuringThreshold=1 \
+     -jar server.jar nogui

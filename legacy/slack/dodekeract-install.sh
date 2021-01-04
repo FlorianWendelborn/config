415dae6792de93d9b7a048dd17310d8e7225ffbb
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2021-01-04 16:06:23 +0100

    delete slack

--- legacy/slack/dodekeract-install.sh
+++
@@ -1,5 +0,0 @@
-echo "Slack Token?"
-read SLACK_TOKEN
-
-docker run --name dodekeract-slackin -e SLACK_ORG=dodekeract -e SLACK_TOKEN="$SLACK_TOKEN" -p 55001:3000 -d chk1/slackin
-sudo cp dodekeract-nginx.conf /srv/docker/nginx/config/sites-enabled/dodekeract-nginx.conf
--- legacy/slack/smartflat-install.sh
+++
@@ -1,5 +0,0 @@
-echo "Slack Token?"
-read SLACK_TOKEN
-
-docker run --name smartflat-slackin -e SLACK_ORG=smartfl-at -e SLACK_TOKEN="$SLACK_TOKEN" -p 55000:3000 -d chk1/slackin
-sudo cp smartflat-nginx.conf /srv/docker/nginx/config/sites-enabled/smartflat-nginx.conf
